68dddc923f0085653f0b543ce636d7b37eb7cdd8
xupenghu <xupenghu@outlook.com>
2018-04-18 18:05:49 +0800

    add lesson7_4

---
+++ lesson7_3邮箱的清空和消除/sourse/t_mbox.c
@@ -0,0 +1,210 @@
+
+#include "tinyOS.h"
+
+
+//邮箱初始化
+void t_mbox_init(t_mbox * mbox, uint32_t max_count, void ** msgbuffer)
+{
+	t_event_init(&mbox->event, t_event_type_mbox);
+	
+	mbox->count = 0;
+	mbox->write = 0;
+	mbox->read = 0;
+	mbox->msgbuffer = msgbuffer;
+	mbox->max_count = max_count;
+
+}
+
+/**********************************************************************************************************
+** Function name        :   tMboxWait
+** Descriptions         :   等待邮箱, 获取一则消息
+** parameters           :   mbox 等待的邮箱
+** parameters           :   msg 消息存储缓存区
+** parameters           :   waitTicks 最大等待的ticks数，为0表示无限等待
+** Returned value       :   等待结果,tErrorResourceUnavaliable.tErrorNoError,tErrorTimeout
+***********************************************************************************************************/
+uint32_t t_mbox_wait (t_mbox * mbox, void **msg, uint32_t wait_ticks) 
+{
+	uint32_t status = t_task_enter_critical();
+	
+	//如果邮箱里有消息，则直接将消息取出来返回
+	if(mbox->count > 0)
+	{
+		* msg = mbox->msgbuffer[mbox->read++];
+		--mbox->count;
+		//如果读到末尾了 则再将读索引移到头部
+		if(mbox->read > mbox->max_count)
+		{
+			mbox->read = 0;
+		}
+		
+		t_task_exit_critical(status);
+		return t_error_no_error;
+	}
+	else
+	{
+		//否则将当前任务加入到等待邮箱的等待队列中去
+		t_event_wait(&mbox->event, currentTask, (void*)0, t_event_type_mbox, wait_ticks);
+		t_task_exit_critical(status);
+		//执行一次任务调度 切换到其他任务
+		t_task_sched();
+		
+		//运行到这里说明已经等到消息了，所以就直接从当前任务中读取等到的消息
+		* msg = currentTask->event_msg;
+		
+		return currentTask->wait_event_resalt;
+
+	}
+
+}
+
+/**********************************************************************************************************
+** Function name        :   tMboxNoWaitGet
+** Descriptions         :   获取一则消息，如果没有消息，则立即退回
+** parameters           :   mbox 获取消息的邮箱
+** parameters           :   msg 消息存储缓存区
+** Returned value       :   获取结果, tErrorResourceUnavaliable.tErrorNoError
+***********************************************************************************************************/
+uint32_t t_mbox_no_wait_get (t_mbox * mbox, void **msg)
+{
+	uint32_t status = t_task_enter_critical();
+	if(mbox->count > 0)
+	{
+		* msg = mbox->msgbuffer[mbox->read++];
+		--mbox->count;
+		if(mbox->read > mbox->max_count)
+		{
+			mbox->read = 0;
+		}
+		
+		t_task_exit_critical(status);
+		return t_error_no_error;
+	}	
+	else
+	{
+			t_task_exit_critical(status);
+			return t_error_resource_unavaliable;
+	}
+	
+}
+
+/**********************************************************************************************************
+** Function name        :   tMboxNotify
+** Descriptions         :   通知消息可用，唤醒等待队列中的一个任务，或者将消息插入到邮箱中
+** parameters           :   mbox 操作的信号量
+** parameters           :   msg 发送的消息
+** parameters           :   notifyOption 发送的选项
+** Returned value       :   tErrorResourceFull
+***********************************************************************************************************/
+uint32_t t_mbox_notify (t_mbox * mbox, void * msg, uint32_t notify_option)
+{
+	uint32_t status = t_task_enter_critical();
+	
+	//如果有任务在等待
+	if(t_event_wait_count(&mbox->event) > 0)
+	{
+		//直接唤醒该任务到调度队列中去，注意唤醒该任务时已经将msg传入该任务的event_msg字段了
+		tTask * task = t_event_wake_up(&mbox->event, (void * )msg, t_error_no_error);
+		//如果该任务的优先级比当前任务的优先级高，则直接调度过去执行该任务
+		if(task->prio < currentTask->prio)
+		{
+			t_task_sched();
+		}
+	}
+	else
+	{
+		//如果计数缓冲区满了 则直接退出
+		if(mbox->count > mbox->max_count)
+		{
+			t_task_exit_critical(status);
+			return t_error_resoure_full;
+		}
+		
+		//如果是按照优先级发送的 则直接在read索引处写值
+		if(notify_option & tMBOXSendFront)
+		{
+			if(mbox->read <= 0)
+			{
+				mbox->read = mbox->max_count - 1;
+			}
+			else
+			{
+				mbox->read --;
+			}
+			
+			mbox->msgbuffer[mbox->read] = msg;
+
+		}
+		else //否则就在write索引处写值
+		{
+			mbox->msgbuffer[mbox->write++] = msg ;
+			
+			if(mbox->write > mbox->max_count)
+			{
+				mbox->write = 0;
+			}
+			
+		}
+		//消息计数量＋＋
+		mbox->count ++;
+		
+	}
+	
+	t_task_exit_critical(status);
+	return t_error_no_error;
+}
+
+/**********************************************************************************************************
+** Function name        :   tMboxFlush
+** Descriptions         :   清空邮箱中所有消息
+** parameters           :   mbox 等待清空的邮箱
+** Returned value       :   无
+***********************************************************************************************************/
+void t_mbox_flush (t_mbox * mbox)
+{
+	uint32_t status = t_task_enter_critical();
+	
+	//如果等待队列不为0 则说明邮箱已经是空的了 不需要再清空
+	if(t_event_wait_count(&mbox->event) == 0)
+	{
+		mbox->count = 0;
+		mbox->read = 0;
+		mbox->write = 0;
+	}
+	
+	t_task_exit_critical(status);
+}
+
+
+/**********************************************************************************************************
+** Function name        :   tMboxDestroy
+** Descriptions         :   销毁邮箱
+** parameters           :   mbox 需要销毁的邮箱
+** Returned value       :   因销毁该邮箱而唤醒的任务数量
+***********************************************************************************************************/
+uint32_t t_mbox_destroy (t_mbox * mbox)
+{
+	uint32_t status = t_task_enter_critical();
+	
+	// 清空事件控制块中的所有任务
+	uint32_t count = t_event_remove_all(&mbox->event, (void *)0, t_error_del);
+	
+	t_task_exit_critical(status);
+	
+	//如果任务大于0，则执行一次调度
+	if(count > 0)
+	{
+		t_task_sched();
+	}
+
+	return count;
+	
+}
+
+
+
+
+
+
+
+
---
+++ lesson7_4邮箱状态查询/switch.c
@@ -0,0 +1,89 @@
+#include "tinyOS.h"
+#include <ARMCM3.h>
+
+
+
+
+#define NVIC_INT_CTRL			0xE000ED04
+#define NVIC_PENDSVSET		0x10000000
+#define NVIC_SYSPRI2			0xE000ED22
+#define NVIC_PENDSV_PRI		0x000000FF
+
+#define MEM32(addr)		*(volatile unsigned long *)(addr)
+#define MEM8(addr)		*(volatile unsigned char *)(addr)
+
+
+void tTaskRunFirst()
+{
+	__set_PSP(0);  													//用于区别是第一个任务跑起来了，还是在进行任务切换
+	MEM8(NVIC_SYSPRI2) = NVIC_PENDSV_PRI;		//设置为最低优先级
+	MEM32(NVIC_INT_CTRL) = NVIC_PENDSVSET;	//触发PendSV中断
+}
+
+void tTaskSwitch()
+{
+
+	MEM32(NVIC_INT_CTRL) = NVIC_PENDSVSET;	//触发PendSV中断
+}
+
+
+
+
+__asm void PendSV_Handler(void)
+{
+	IMPORT currentTask
+	IMPORT nextTask
+	
+	MRS R0, PSP			//获取当前任务的堆栈指针
+	CBZ	R0, PendSV_Handler_nosave
+	
+	STMDB	R0!, {R4-R11} //保存除异常中自动保存的寄存器外其他的寄存器值自动保存起来{R4-R11}
+	
+	LDR R1, =currentTask
+	LDR R1, [R1]
+	STR R0, [R1]		//保存好后，将最后的堆栈栈顶位置，保存到currentTask->stack处，相当于完成了一次当前任务的压栈操作
+	
+PendSV_Handler_nosave
+	
+	LDR		R0, =currentTask
+	LDR 	R1, =nextTask
+	LDR		R2, [R1]
+	STR 	R2, [R0]				//先将currentTask切换成nextTask 也就是下一任务变成了当前任务
+	
+	LDR		R0, [R2]
+	LDMIA	R0!, {R4-R11}
+		
+	MSR	PSP,	R0
+	ORR	LR,	LR, #0x04
+	BX	LR
+	
+}
+
+/**********************************************************************************************************
+** Function name        :   tTaskEnterCritical
+** Descriptions         :   进入临界区
+** parameters           :   无
+** Returned value       :   进入之前的临界区状态值
+***********************************************************************************************************/
+uint32_t t_task_enter_critical (void) 
+{
+    uint32_t primask = __get_PRIMASK();
+    __disable_irq();        // CPSID I
+    return primask;
+}
+
+/**********************************************************************************************************
+** Function name        :   tTaskExitCritical
+** Descriptions         :   退出临界区,恢复之前的临界区状态
+** parameters           :   status 进入临界区之前的CPU
+** Returned value       :   进入临界区之前的临界区状态值
+***********************************************************************************************************/
+void t_task_exit_critical (uint32_t status) 
+{
+    __set_PRIMASK(status);
+}
+
+
+
+
+
